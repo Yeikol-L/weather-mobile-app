e8f6e2571cc9eff8f1f6a68301585b50a62ea09f
Yeikol Lorenzo <yeigamer1@gmail.com>
2025-01-14 20:25:54 -0400

    initial files

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/services/location.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            determinePosition();
+            //Get the current location
+          },
+          child: Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
